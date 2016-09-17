be9b315313d6b1c423023d4920cf84b7855aa06a
PrestonLabsAndTech <sam@prestonlabsandtechnology.com>
2016-09-16 20:56:22 -0600

    Starts in the center of the screen now

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -106,6 +106,7 @@
             this.Controls.Add(this.irfButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
